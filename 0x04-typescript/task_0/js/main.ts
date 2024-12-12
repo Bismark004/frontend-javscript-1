870b44f5656ff3449db6e47d695c49531949b64e
Bismark Adzai <adzaibismark@gmail.com>
2024-12-12 18:42:11 +0000

    commit main.ts

---
+++ 0x04-typescript/task_0/js/main.ts
@@ -0,0 +1,50 @@
+interface student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const students1: student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 18,
+    location: "New York",
+
+};
+
+const student2: student = {
+    firstName: "Jane",
+    lastName: "Smith",
+    age: 22,
+    location: "Carlifornia",
+}
+
+const studentList: student[] = [students1, student2];
+
+const table = document.createElement("table");
+const headerRow = document.createElement("tr");
+
+const headers =["First Name", "Location"];
+headers.forEach((header) => {
+    const th = document.createElement("th");
+    th.textContent = header;
+    headerRow.appendChild(th);    
+});
+
+table.appendChild(headerRow);
+
+
+studentList.forEach((student) => {
+    const row = document.createElement("tr");
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = student.firstName;
+    row.appendChild(firstNameCell);
+
+    const locationCell = document.createElement("td");
+    locationCell.textContent = student.location;
+    row.appendChild(locationCell);
+}
+
+document.body.appendChild(table);
+
